13864f9771103159ad0043aaaf1f397d82acd395
Aaron Merlob <aaron@mountain213.com>
2015-08-23 13:23:16 -0700

    added api data grabber file

---
+++ api_surveymonkey.py
@@ -0,0 +1,11 @@
+"""
+This class grabs survey data from the surveymonkey API
+and stores it somewhere
+"""
+__author__="Aaron"
+__date__ ="$Aug 23, 2015 1:06:52 PM$"
+
+
+
+if __name__ == "__main__":
+    print "Hello World";
