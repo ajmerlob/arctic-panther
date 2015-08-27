76a21e63e8339b21c982c0513c0de5285d09069c
Aaron Merlob <aaron@mountain213.com>
2015-08-27 11:56:23 -0700

    added new top level main class

---
+++ main.py
@@ -0,0 +1,14 @@
+from pair.main import Main
+from core.config import Config
+
+if __name__ == "__main__":
+    spam_missing_surveys = False
+
+
+    for group_id in Config.groups:
+        main = Main(group_id)
+        main.get_users()
+        if spam_missing_surveys:
+            main.send_missing_survey_messages()
+        pairs = main.analyze_pairs()
+#        main.send_pair_assignment_messages("May 29th", pairs,False)
